5295eaea2b98cab577a0f479a992d244867e193a
Jonathan Thomas <jt@captainill.com>
2012-06-09 23:07:27 -0700

    add twitter callback to env

--- config.js
+++ config.js
@@ -3,8 +3,7 @@ exports.config = {
 		"twitter": {
 			"consumerkey" : "wovpSUMWpwLB2g9lKafxg",
 			"consumersecret" : "TAMz3AH45WkI0NXjijchU1lYJsoy3SesiCD04xUiQs4",
-			"callback" : "http://127.0.0.1:5000/auth/twitter_callback",
-			"livecallback" : "http://photungle.herokuapp.com/"
+			"callback" : "http://photungle.herokuapp.com/",
 		}
 	},
 	"theme": {
